90d94fd1ee985cd342bf5a4ecac4002ab672964a
strikersix23 <17087430+strikersix23@users.noreply.github.com>
2025-11-16 01:47:02 +0000

    Deploying to gh-pages from @ strikersix23/SkyEmu@246ba26421fc50d14b04979f00b384c98bb01d31 🚀

--- branch/dev/sw.js
+++ branch/dev/sw.js
@@ -1,5 +1,5 @@
 // Define the cache version
-const CACHE_VERSION = 'd5fd65ed6ddb06847cb920c7f65f3bba6f3b9c63';
+const CACHE_VERSION = '246ba26421fc50d14b04979f00b384c98bb01d31';
 
 // Define an array of URLs to cache
 const CACHE_URLS = [
